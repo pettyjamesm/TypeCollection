4ff27786b60b0bcfdc6e93b2b0fa70437ce5e6e3
James Petty <jp@jibe.com>
2012-01-10 11:56:33 -0500

    Another fix

--- lib/typecollection/class+inferred_type.rb
+++ lib/typecollection/class+inferred_type.rb
@@ -1,18 +1,14 @@
 class Class
   def inferred_type()
-    if (!defined?(@__tc_inferred_type))
-      klass_name   = self.name.split("::").last
-      parent_klass = self.superclass
-      while(parent_klass != nil)
-        check = parent_klass.name.split("::").last
-        if (klass_name.match(check))
-          @__tc_inferred_type = klass_name.gsub(/#{check}\z/, "")
-          break
-        end
-        parent_klass = parent_klass.superclass
+    klass_name   = self.name.split("::").last
+    parent_klass = self.superclass
+    while(parent_klass != nil)
+      check = parent_klass.name.split("::").last
+      if (klass_name.match(check))
+        return klass_name.gsub(/#{check}\z/, "")
       end
-      @__tc_inferred_type = nil
+      parent_klass = parent_klass.superclass
     end
-    return @__tc_inferred_type
+    return nil
   end
 end
